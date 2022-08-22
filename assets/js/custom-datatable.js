f9616799fdfc7ab950926ceab2262c65c0f78d78
permafrost06 <zahin06@gmail.com>
2022-08-22 15:19:31 +0600

    changed column name output

--- assets/js/custom-datatable.js
+++ assets/js/custom-datatable.js
@@ -1,39 +1,16 @@
-// jQuery(document).ready(function () {
-//   jQuery(".custom-datatable").DataTable({
-//     ordering: false,
-//     searching: false,
-//     processing: true,
-//     serverSide: true,
-//     ajax: {
-//       url: customDatatableAjax.ajax_url,
-//       data: function (d) {
-//         d.action = "dtm_get_datatable_rows";
-//         d._ajax_nonce = customDatatableAjax.nonce;
-//         d.table_id = "3";
-//       },
-//     },
-//     columns: [
-//       { data: "name" },
-//       { data: "email" },
-//       { data: "phone" },
-//       { data: "address" },
-//     ],
-//   });
-// });
-
 jQuery(document).ready(function () {
   jQuery(".custom-datatable").each((idx, elem) => {
-    const table_id = jQuery(elem).data("custom_datatable_id");
-    const columns = jQuery(elem)
-      .data("custom_datatable_columns")
-      .split(", ")
-      .map((col) => {
-        return {
-          data: col,
-        };
-      });
+    const el = elem;
+    elem = jQuery(elem);
+    const table_id = elem.data("custom_datatable_id");
 
-    jQuery(elem).DataTable({
+    const columns = [];
+    elem.find("th").each((idx, thEl) => {
+      const column_value = jQuery(thEl).data("custom_datatable_column_value");
+      columns.push({ data: column_value });
+    });
+
+    elem.DataTable({
       ordering: false,
       searching: false,
       processing: true,
@@ -46,7 +23,7 @@ jQuery(document).ready(function () {
           d.table_id = table_id;
         },
       },
-      columns: columns,
+      columns,
     });
   });
 });
--- includes/Frontend/views/table-view.php
+++ includes/Frontend/views/table-view.php
@@ -1,13 +1,8 @@
-<?php
-$column_vals = array_map(function ($column) {
-  return $column->value;
-}, $data['columns']);
-?>
-<table class="custom-datatable" data-custom_datatable_id="<?php esc_attr_e($data['table_id']); ?>" data-custom_datatable_columns="<?php esc_attr_e(implode(', ', $column_vals)); ?>" style="width:100%">
+<table class="custom-datatable" data-custom_datatable_id="<?php esc_attr_e($data['table_id']); ?>" style="width:100%">
   <thead>
     <tr>
       <?php foreach ($data['columns'] as $column) : ?>
-        <th><?php esc_html_e($column->label) ?></th>
+        <th data-custom_datatable_column_value="<?php esc_attr_e($column->value) ?>"><?php esc_html_e($column->label) ?></th>
       <?php endforeach ?>
     </tr>
   </thead>
