79e447c6ea0b82decd8d0c38ebabf9b27e9019f8
permafrost06 <zahin06@gmail.com>
2022-08-22 19:48:00 +0600

    added form validation fixed style

--- src/views/AddNewTableView.vue
+++ src/views/AddNewTableView.vue
@@ -1,52 +1,96 @@
 <script setup>
-import { ref } from "vue";
+import { ref, reactive } from "vue";
 import { postAJAX } from "../composable";
 
-const table_name = ref("");
+const newTable = ref({
+  table_name: "",
+  columns: [
+    {
+      label: "",
+      value: "",
+    },
+  ],
+});
 
-const columns = ref([]);
+const formEl = ref();
 
 const addCol = () => {
-  columns.value.push({
-    value: "",
+  newTable.value.columns.push({
     label: "",
+    value: "",
   });
 };
 
 const onSubmit = async () => {
-  try {
-    const { success } = await postAJAX("add_table", {
-      table_name: table_name.value,
-      columns: JSON.stringify(columns.value),
-    });
+  formEl.value.validate(async (valid) => {
+    if (valid) {
+      try {
+        const { success } = await postAJAX("add_table", {
+          table_name: newTable.value.table_name,
+          columns: JSON.stringify(newTable.value.columns),
+        });
 
-    if (success) console.log("successfully added table");
-    else console.log("couldn't add table");
-  } catch (e) {
-    console.log(e);
-  }
+        if (success) console.log("successfully added table");
+        else console.log("couldn't add table");
+      } catch (e) {
+        console.log(e);
+      }
+    } else {
+      console.log("Form not valid");
+    }
+  });
 };
+
+const formRules = reactive({
+  table_name: [
+    {
+      required: true,
+      message: "Please enter a name for the table",
+      trigger: "blur",
+    },
+  ],
+});
 </script>
 
 <template>
-  <el-form @submit.prevent="onSubmit" label-width="100px">
+  <el-form
+    @submit.prevent="onSubmit"
+    :rules="formRules"
+    :model="newTable"
+    label-width="auto"
+    ref="formEl"
+  >
     <el-form-item prop="table_name" label="Table Name">
       <el-col :span="8">
-        <el-input v-model="table_name" />
+        <el-input v-model="newTable.table_name" />
       </el-col>
     </el-form-item>
-    <el-form-item
-      :label="'Column ' + (index + 1)"
-      v-for="(column, index) in columns"
-      :key="index"
-    >
-      <el-col :span="4">
+    <el-row v-for="(column, index) in newTable.columns" :key="index">
+      <el-form-item
+        class="inline-form-item"
+        :prop="'columns.' + index + '.label'"
+        :label="'Column ' + (index + 1)"
+        :rules="{
+          required: true,
+          message: 'Please enter a label for the column',
+          trigger: 'blur',
+        }"
+      >
         <el-input v-model="column.label" placeholder="Label" />
-      </el-col>
-      <el-col :span="4">
+      </el-form-item>
+      <el-form-item
+        class="inline-form-item"
+        :prop="'columns.' + index + '.value'"
+        :rules="{
+          required: true,
+          message: 'Please enter a key for the column',
+          trigger: 'blur',
+        }"
+        label-width="0px"
+      >
         <el-input v-model="column.value" placeholder="Key" />
-      </el-col>
-    </el-form-item>
+      </el-form-item>
+    </el-row>
     <el-form-item>
       <el-button @click="addCol">+ Add column</el-button>
     </el-form-item>
@@ -58,3 +102,12 @@ const onSubmit = async () => {
     </el-form-item>
   </el-form>
 </template>
+
+<style lang="scss" scoped>
+.inline-form-item {
+  display: inline-flex;
+  vertical-align: middle;
+  margin-right: 32px;
+  margin-bottom: 32px;
+}
+</style>
