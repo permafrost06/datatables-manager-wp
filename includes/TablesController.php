7e64206d2cf31fff35aeb6da412cf9a0ffdbe908
permafrost06 <zahin06@gmail.com>
2022-08-31 16:37:51 +0600

    Added Datatable processing class moved filtering and ordering to DTProcessing class

--- includes/TablesController.php
+++ includes/TablesController.php
@@ -157,37 +157,9 @@ class TablesController
       $result['row_id'] = $row_id;
     }
 
-    if (isset($search['value']) && $search['value'] != '') {
-      $results = array_values(array_filter($results, function ($result) use ($search) {
-        foreach ($result as $key => $value) {
-          if ($key != 'row_id') {
-            if (strpos($value, $search['value']) !== false) {
-              return true;
-            }
-          }
-        }
+    DTProcessing::filter($results, $search);
 
-        return false;
-      }));
-    }
-
-    $columns_data = array_map(function ($column) {
-      return $column['data'];
-    }, $columns);
-
-    $order_by = $columns_data[$order[0]['column']];
-    $order_dir = $order[0]['dir'];
-
-    usort($results, function ($a, $b) use ($order_by, $order_dir) {
-      $value_a = $a[$order_by];
-      $value_b = $b[$order_by];
-
-      if ($order_dir == 'asc') {
-        return strcmp($value_a, $value_b);
-      } else {
-        return strcmp($value_b, $value_a);
-      }
-    });
+    DTProcessing::order($results, $columns, $order);
 
     return $results;
   }
