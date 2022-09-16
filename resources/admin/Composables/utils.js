fc36a207ec050c0729a014133039ebbc11b95b06
permafrost06 <zahin06@gmail.com>
2022-09-16 16:10:21 +0600

    fixed getRestError function

--- resources/admin/Composables/utils.js
+++ resources/admin/Composables/utils.js
@@ -15,6 +15,7 @@ export const errorMessage = (message) => {
 };
 
 export const getRestError = (e) => {
-  if (e.responseJSON) return responseJSON.code + " - " + responseJSON.message;
+  if (e.responseJSON)
+    return e.responseJSON.code + " - " + e.responseJSON.message;
   else return e.responseText;
 };
